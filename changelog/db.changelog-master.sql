751ff3e1b5583a38992c08cd0ac8a386546333d7
Renzo <rcchahuac@ucvvirtual.edu.pe>
2026-09-19 00:06:13 -0500

    Update schemas in changelog for student 7002798705

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -1,11 +1,11 @@
 --liquibase formatted sql
 
 --changeset estudiante:001
-CREATE SCHEMA IF NOT EXISTS <CATALOGO>.bi_lab_<codigo_estudiante>
+CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_7002798705
 COMMENT 'Schema principal - BI and Big Data - Lab 02';
---rollback DROP SCHEMA IF EXISTS <CATALOGO>.bi_lab_<codigo_estudiante>;
+--rollback DROP SCHEMA IF EXISTS workspace.bi_lab_7002798705;
 
 --changeset estudiante:002
-CREATE SCHEMA IF NOT EXISTS <CATALOGO>.bi_staging_<codigo_estudiante>
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_7002798705
 COMMENT 'Staging schema - BI and Big Data - Lab 03';
---rollback DROP SCHEMA IF EXISTS <CATALOGO>.bi_staging_<codigo_estudiante>;
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_7002798705;
